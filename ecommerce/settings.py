13a77fb548453da1e371861ee954776aaed97cd1
pooja <poojachauhan2102@gmail.com>
2021-12-31 03:26:48 -0800

    stsic change

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -143,7 +143,7 @@ MEDIA_URL='/media/'
 
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static_in_env')
+    os.path.join(BASE_DIR,'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
