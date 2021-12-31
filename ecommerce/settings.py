2fc0d2a15681692d6c24ed6b16d5329e70a5728f
pooja <poojachauhan2102@gmail.com>
2021-12-31 03:40:18 -0800

    chnagesee

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -143,7 +143,7 @@ MEDIA_URL='/media/'
 
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'static_in_env')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
