43d84c7cb8296bb81ba1472d6006e28b92f706e6
pooja <poojachauhan2102@gmail.com>
2022-01-02 03:40:54 -0800

    chsfg

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,11 +22,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('shop.urls'))
-]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+      
 
 
 
