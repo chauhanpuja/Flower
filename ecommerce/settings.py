28f15eec2a8ebe54149b3c1146eca3bc420cf357
pooja <poojachauhan2102@gmail.com>
2022-01-03 21:19:40 -0800

    cadded IPaddress

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-70si&)q_&fsp_o-wi+2a^muik^!&c4re2)7n0$(@tju@o5o1wj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['165.227.218.23']
 
 
 # Application definition
